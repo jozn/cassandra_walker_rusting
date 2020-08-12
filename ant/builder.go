8d4c51b31a1dc285142feaf0dd49334a38659415
Hamid <7atashfeshan@gmail.com>
2020-08-12 14:54:49 +0430

    Reorgnazie Go build codes

--- ant/builder.go
+++ ant/builder.go
@@ -7,65 +7,25 @@ import (
 	"text/template"
 )
 
-func buildRust(gen *GenOut) {
-	os.MkdirAll(OUTPUT_DIR_RUST_X, os.ModePerm)
-
-	writeOutputRust("rpc.rs", buildFromTemplate("rpc.rs", gen))
-}
-
-func buildGo(gen *GenOut) {
-	os.MkdirAll(OUTPUT_DIR_GO_X, os.ModePerm)
+func build_old(gen *GenOut) {
 	os.MkdirAll(OUTPUT_DIR_GO_X_CONST, os.ModePerm)
-	os.MkdirAll(OUTPUT_ANDROID_APP_DIR, os.ModePerm)
-	os.MkdirAll(OUTPUT_ANDROID_PROTO_MOUDLE_DIR, os.ModePerm)
+	//os.MkdirAll(OUTPUT_DIR_GO_X, os.ModePerm)
+	//os.MkdirAll(OUTPUT_ANDROID_APP_DIR, os.ModePerm)
+	//os.MkdirAll(OUTPUT_ANDROID_PROTO_MOUDLE_DIR, os.ModePerm)
 
 	OutGoRPCsStr := buildFromTemplate("rpc.tgo", gen)
-	writeOutputGo("rpc.go", OutGoRPCsStr)
+	writeOutputGo("pb__gen_ant.go", OutGoRPCsStr)
 
 	OutGoRPCsEmptyStr := buildFromTemplate("rpc_empty_imple.tgo", gen)
-	writeOutputGo("empty.go", OutGoRPCsEmptyStr)
+	writeOutputGo("pb__gen_ant_empty.go", OutGoRPCsEmptyStr)
+
 	writeOutputGo("pb__gen_enum.proto", buildFromTemplate("enums.proto", gen))
 	writeOutputGo("RPC_HANDLERS.java", buildFromTemplate("RPC_HANDLERS.java", gen))
 	writeOutputGo("PBFlatTypes.java", buildFromTemplate("PBFlatTypes.java", gen))
 	writeOutputGo("flat.go", buildFromTemplate("flat.tgo", gen))
 	writeOutputConstantGo("pb.go", buildFromTemplate("xconst.tgo", gen))
+
 	writeOutputGo("rpc_client.go", buildFromTemplate("rpc_client.tgo", gen))
-
-	build_old(gen)
-
-}
-
-func writeOutputGo(fileName, output string) {
-	err := ioutil.WriteFile(OUTPUT_DIR_GO_X+fileName, []byte(output), os.ModePerm)
-	noErr(err)
-}
-
-func writeOutputConstantGo(fileName, output string) {
-	ioutil.WriteFile(OUTPUT_DIR_GO_X_CONST+fileName, []byte(output), os.ModePerm)
-}
-
-func writeOutputRust(fileName, output string) {
-	err := ioutil.WriteFile(OUTPUT_DIR_RUST_X+fileName, []byte(output), os.ModePerm)
-	noErr(err)
-}
-///////////////////// Archives /////////////////
-
-func build_old(gen *GenOut) {
-	os.MkdirAll(OUTPUT_DIR_GO_X_CONST, os.ModePerm)
-
-	OutGoRPCsStr := buildFromTemplate("rpc.tgo", gen)
-	writeOutput("pb__gen_ant.go", OutGoRPCsStr)
-
-	OutGoRPCsEmptyStr := buildFromTemplate("rpc_empty_imple.tgo", gen)
-	writeOutput("pb__gen_ant_empty.go", OutGoRPCsEmptyStr)
-
-	writeOutput("pb__gen_enum.proto", buildFromTemplate("enums.proto", gen))
-	writeOutput("RPC_HANDLERS.java", buildFromTemplate("RPC_HANDLERS.java", gen))
-	writeOutput("PBFlatTypes.java", buildFromTemplate("PBFlatTypes.java", gen))
-	writeOutput("flat.go", buildFromTemplate("flat.tgo", gen))
-	writeOutputConstant("pb.go", buildFromTemplate("xconst.tgo", gen))
-
-	writeOutput("rpc_client.go", buildFromTemplate("rpc_client.tgo", gen))
 
 	//////////////// For Android /////////////
 	writeOutputAndroidProto("RPC_HANDLERS.java", buildFromTemplate("RPC_HANDLERS.java", gen))
@@ -81,6 +41,27 @@ func build_old(gen *GenOut) {
 
 }
 
+func writeOutputGo(fileName, output string) {
+	err := ioutil.WriteFile(OUTPUT_DIR_GO_X+fileName, []byte(output), os.ModePerm)
+	noErr(err)
+}
+
+func writeOutputConstantGo(fileName, output string) {
+	err := ioutil.WriteFile(OUTPUT_DIR_GO_X_CONST+fileName, []byte(output), os.ModePerm)
+	noErr(err)
+}
+
+func writeOutputAndroidProto(fileName, output string) {
+	err := ioutil.WriteFile(OUTPUT_ANDROID_PROTO_MOUDLE_DIR+fileName, []byte(output), os.ModePerm)
+	noErr(err)
+}
+
+func writeOutputAndroidApp(fileName, output string) {
+	err := ioutil.WriteFile(OUTPUT_ANDROID_APP_DIR+fileName, []byte(output), os.ModePerm)
+	noErr(err)
+}
+
+////////////// Shared /////////////
 func buildFromTemplate(tplName string, gen *GenOut) string {
 	tpl := template.New("go_interface" + tplName)
 	tpl.Funcs(fns)
@@ -94,24 +75,4 @@ func buildFromTemplate(tplName string, gen *GenOut) string {
 	noErr(err)
 
 	return buffer.String()
-}
-
-func writeOutput(fileName, output string) {
-	err := ioutil.WriteFile(OUTPUT_DIR_GO_X+fileName, []byte(output), os.ModePerm)
-	noErr(err)
-}
-
-func writeOutputConstant(fileName, output string) {
-	err := ioutil.WriteFile(OUTPUT_DIR_GO_X_CONST+fileName, []byte(output), os.ModePerm)
-	noErr(err)
-}
-
-func writeOutputAndroidProto(fileName, output string) {
-	err := ioutil.WriteFile(OUTPUT_ANDROID_PROTO_MOUDLE_DIR+fileName, []byte(output), os.ModePerm)
-	noErr(err)
-}
-
-func writeOutputAndroidApp(fileName, output string) {
-	err := ioutil.WriteFile(OUTPUT_ANDROID_APP_DIR+fileName, []byte(output), os.ModePerm)
-	noErr(err)
 }
---
+++ ant/builder_rust.go
@@ -0,0 +1,18 @@
+package ant
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func buildRust(gen *GenOut) {
+	os.MkdirAll(OUTPUT_DIR_RUST_X, os.ModePerm)
+
+	writeOutputRust("rpc.rs", buildFromTemplate("rpc.rs", gen))
+}
+
+func writeOutputRust(fileName, output string) {
+	err := ioutil.WriteFile(OUTPUT_DIR_RUST_X+fileName, []byte(output), os.ModePerm)
+	noErr(err)
+}
+
