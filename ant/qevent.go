4dce713bd29c9bee50015487ecde5eb5c59fc39b
Hamid <7atashfeshan@gmail.com>
2021-01-11 16:29:27 +0330

    Remove old code of QEvent processing

--- ant/qevent.go
+++ ant/qevent.go
@@ -2,7 +2,6 @@ package ant
 
 import (
 	"fmt"
-	"os"
 )
 
 func makeQEventStruct(gen *GenOut) (qEventServices []QEventService) {
@@ -67,60 +66,4 @@ func _buildQEventField(pramsMsgName string, gen *GenOut) (out []QEventPBFields) 
 		out = append(out, msgEventField)
 	}
 	return
-}
-
-
-////////////////////////////////// bk ///////////////////////////////////
-func makeQvent_bk(gen *GenOut) {
-	var qventServices []QEventService
-
-	for i := 0; i < len(gen.Services); i++ {
-		ser := gen.Services[i]
-		qeventSer := QEventService{
-			ServiceName: ser.NameStriped,
-			Events:      nil,
-		}
-
-		for j := 0; j < len(ser.Methods); j++ {
-			method := ser.Methods[j]
-			qevent:= QEvent{
-				EventName: method.MethodNameStriped,
-				TagNum:    method.Pos + 5,
-				Fields:    nil,
-			}
-
-			// Add QEvents fileds for building message
-			fondMsgParam := false
-			for m := 0; m < len(gen.Messages); m++ {
-				msg := gen.Messages[m]
-				if msg.Name == method.GoInTypeName {
-					fondMsgParam = true
-					// Extract each pb message fields
-					for g := 0; g < len(msg.Fields); g++ {
-						msgFiled := msg.Fields[g]
-
-						msgEventField := QEventPBFields{
-							Name: msgFiled.FieldName    ,
-							PBType:   msgFiled.TypeName,
-							Repeated: msgFiled.Repeated,
-						}
-
-						qevent.Fields = append(qevent.Fields, msgEventField)
-
-					}
-					break // found the msg exit
-				}
-			}
-			if !fondMsgParam {
-				fmt.Printf("did not found pb param message: %s\n", method.GoInTypeName)
-				os.Exit(1)
-			}
-
-
-			qeventSer.Events = append(qeventSer.Events, qevent)
-		}
-		qventServices = append(qventServices, qeventSer)
-	}
-
-	PrettyPrint(qventServices[2])
 }
